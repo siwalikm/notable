cd288f0ff9907128c38e7b7c11dc8021b287d970
siwalik <siwalik@freshdesk.com>
2017-06-27 04:45:51 +0530

    CRUP API setup

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,8 @@
+var ObjectID = require('mongodb').ObjectID;
+// detailed doc on methods at https://docs.mongodb.com/manual/reference/method/js-collection/
+
 module.exports = function(app, db) {
+// CREATE-r-u-d
   app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
     db.collection('notes').insert(note, (err, result) => {
@@ -9,4 +13,44 @@ module.exports = function(app, db) {
       }
     });
   });
+// c-READ-u-d
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      }
+    });
+  });
+// c-r-UPDATE-d
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').update(details, note, (err, result) => {
+      if (err) {
+          res.send({'error':'An error has occurred'});
+      } else {
+          res.send(note);
+      } 
+    });
+  });
+// c-r-u-DELETE
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send('Note ' + id + ' deleted!');
+      } 
+    });
+  });
+
+
+
 };
