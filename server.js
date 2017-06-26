a3aa6a9bb31c69f19b168c640480e3bbfe5d5d51
siwalikm <siwalikm@gmail.com>
2017-06-27 00:48:00 +0530

    checking port

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ const express     = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser  = require('body-parser');
 
-const app         = express();
+const app   = express();
+const port  = 8000;
+app.listen(port, () => {
+  console.log(`we are live on ${port}`);
+});
