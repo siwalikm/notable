41ab63187cca63eff7e3c8a407c0335356ee1a23
siwalik <siwalik@freshdesk.com>
2017-06-27 03:54:36 +0530

    ignore db.js

--- config/db.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    url : 'mongodb://username:password@xxxxx.com/dbName'
-    // Setup a mongodb account in mlab.com or similar site.
-    // Create a db and then an username & Password
-    // Fill the db url here
-};
-
